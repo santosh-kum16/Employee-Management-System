927b214781e715721a91014edc24cff74ee26562
Santosh Kumar <santosh.kum16@gmail.com>
2021-10-15 23:38:28 +0530

    Initial commit

---
+++ src/test/java/com/exp/demo/EmployeeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exp.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
