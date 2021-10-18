c218120e79f461f887c870aa01651dec9ae2c81b
Santosh Kumar <santosh.kum16@gmail.com>
2021-10-18 22:30:26 +0530

    fetching employee details
    Fetching employee detail by Employee Id if present other wise returning null

--- src/main/java/com/exp/demo/service/EmpService.java
+++ src/main/java/com/exp/demo/service/EmpService.java
@@ -1,6 +1,7 @@
 package com.exp.demo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,4 +23,11 @@ public class EmpService {
 		return repo.findAll();
 	}
 
+	public Employee getEmpById(int id) {
+		Optional<Employee> e = repo.findById(id);
+		if(e.isPresent()) {
+			return e.get();
+		}
+		return null;
+	}
 }
